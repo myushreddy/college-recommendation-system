9d529bf7642623cdd7e5ca2004bd36de599645aa
Ayush Reddy <mayushreddy24@gmail.com>
2026-01-11 10:18:19 +0530

    Create page.tsx

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import ChatInterface from '@/components/ChatInterface'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gradient-to-br from-primary-50 via-white to-blue-50">
+      <ChatInterface />
+    </main>
+  )
+}
